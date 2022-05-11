ed7e2c51965bd3250bfbe84f060250977f8228c1
Dinesh Kumar Baalajee Jothi <83833658+bjdineshkumar@users.noreply.github.com>
2022-05-11 20:03:51 -0300

    Time: 15 ms (5.24%), Space: 54.6 MB (5.06%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -3,20 +3,20 @@ class Solution {
         
         Arrays.sort(nums);
         
-        if(nums[nums.length-1] != nums.length) {
-            return nums.length;
+        Set<Integer> numSet = new HashSet<>();
+        
+        for(Integer num: nums) {
+            numSet.add(num);
         }
         
-        else if(nums[0] != 0) {
-            return 0;
-        }
         
-        for(int i = 1 ; i < nums.length ; i++) {
-            int expected = nums[i-1]+1;
-            if(nums[i] != expected) {
-                return expected;
+        for(int i = 0 ; i < nums.length+1 ; i++) {
+            if(!numSet.contains(i)) {
+                return i;
             }
         }
+        
         return -1;
+        
     }
 }
